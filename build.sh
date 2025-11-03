0744ec44651b371d7bf2e0bbf2b1c42391999a1b
bxavaby <bfernandes4@pm.me>
2025-11-03 21:33:21 +0100

    add: build.sh is a helper script that builds the executables and prints SHA256 checksums to checksums.txt

---
+++ build.sh
@@ -0,0 +1,62 @@
+#! /bin/bash
+
+set -e
+
+#! /bin/bash
+
+set -e
+
+REPO="rnm"
+
+DIR="zig-out/bin"
+
+TARGETS=(
+	"aarch64-linux"
+	"x86_64-linux"
+	"aarch64-macos"
+	"x86_64-macos"
+	"x86_64-windows"
+)
+
+clear
+
+for target in "${TARGETS[@]}"; do
+	ARCH=$(echo $target | cut -d '-' -f 1)
+	OS=$(echo $target | cut -d '-' -f 2)
+	DTARGET="$ARCH-$OS"
+	PROG="$REPO-$OS-$ARCH"
+	OUTPUT="$DIR/$PROG"
+
+	echo "Making $OS-$ARCH..."
+
+	zig build -Dtarget=$DTARGET -Doptimize=ReleaseSmall || {
+		echo -e "\033[31m✗\033[0m Could not make $PROG"
+		sleep 1
+		continue
+	}
+
+	if [ -f "$DIR/$REPO" ]; then
+		mv "$DIR/$REPO" "$OUTPUT"
+	elif [ -f "$DIR/$REPO.exe" ]; then
+		mv "$DIR/$REPO.exe" "$OUTPUT.exe"
+	fi
+	
+	sha256sum "$DIR"/rnm-* > "$DIR/checksums.txt"
+	
+	if [ -f "$OUTPUT" ] || [ -f "$OUTPUT.exe" ]; then
+		echo -e "\033[32m✓\033[0m Made $OUTPUT"
+		echo
+	else
+		echo -e "\033[31m✗\033[0m Output file not found for $PROG"
+		echo
+	fi
+	sleep 1
+done
+
+echo "Making checksums.txt with sha256sum..."
+echo
+sleep 1
+
+sha256sum "$DIR"/rnm-* > "$DIR/checksums.txt"
+
+echo -e "\033[32m✓\033[0m All done!" 
